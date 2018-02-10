6d69614e68f26e69a896a0fc8c3c19c6695c5d39
Aaron <aaron.brown@mac.com>
2018-02-10 16:36:26 -0600

    updated a few items in the setup for transfer to heroku

--- script1.py
+++ script1.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
+    #if you call this script from another script, __name__ is set to script1
 
-@app.route('/')
-def home():
+@app.route('/') #this is a python decorator
+def home(): #must write a function after the decorator above
     return render_template("home.html")
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
+
+
+
 if __name__=="__main__":
+    #since we are calling this script, the internal reference is __main__
     app.run(debug=True)
